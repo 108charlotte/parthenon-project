0000b2b870932dede994f661c91ad493e1aac460
108charlotte <108sarahsmith@gmail.com>
2025-11-16 00:22:06 -0500

    idk what i did

--- minigame.gd
+++ minigame.gd
@@ -1,18 +1,25 @@
-extends CanvasLayer
+extends Node2D
 
+var star_positions: Array[Vector2] = []
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	generate_level()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-func generate_level(): 
-	var num_stars = GlobalVariables.num_stars
-	for i in range(len(num_stars)): 
+func generate_level():
+	star_positions.clear()
+	var num_stars = 5
+	for i in range(num_stars):
 		var star = Sprite2D.new()
 		star.texture = load("res://icon.svg")
+		var pos = Vector2(randi_range(0, 640), randi_range(0, 400))
+		star.position = pos
 		add_child(star)
-	print("done")
+		star_positions.append(pos)
+
+func _draw():
+	for i in range(star_positions.size() - 1):
+		var from = star_positions[i]
+		var to = star_positions[i + 1]
+
+		draw_line(from, to, Color(0, 0, 1), 5)
+		draw_line(from, to, Color(1, 1, 1), 1)
